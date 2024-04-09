270a00c3cec8f7dc29db464b202bde2e09467e0c
Lykexxx <lykas1501@mail.ru>
2024-04-09 19:16:06 +0300

    add-basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  basePath: "/nextjs-sneakers",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
